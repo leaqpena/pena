91bb0e1634e9740d7ffe1b2a15cd57efc7df022d
lea pena <leapena6@gmail.com>
2025-04-29 17:41:33 +0800

    Modified

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,12 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
     }
+    protected function Authenticated(Request $request, $user)
+    {
+        if ($user->role === 'admin'){
+            return redirect ('/admin/dashboard');
+        }else{
+            return redirect ('/user/dashboard');
+        }
+    }
 }
